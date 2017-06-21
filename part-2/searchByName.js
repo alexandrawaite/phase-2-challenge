f9e14e0dfad1c8a2854cfb6a18166d811d0f0229
alexandrawaite <alexandrawaite@gmail.com>
2017-06-21 11:38:20 -0700

    start part 2

---
+++ part-2/searchByName.js
@@ -0,0 +1,12 @@
+// Define JSON file
+var fs = require("fs");
+// Get content from file
+var contents = fs.readFileSync("clients.json");
+
+var contents = require("./clients.json")
+// Define to JSON type
+var jsonContent = JSON.parse(contents);
+
+
+
+ console.log("Rep Name:", jsonContent.rep_name);
